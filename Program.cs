9f9fefb66fea212d4d681ac7476806b1d2372693
VitoriaRossi <vitoria.rossi555@gmail.com>
2023-10-29 19:46:21 -0300

    Aprendi sobre variáveis, constantes e tipos de dados.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+string nome = "Vitória";
+string sobrenome = "Rossi";
+const string naturalidade = "Brasileira";
+
+int idade;
+idade = 19;
+
+decimal altura; 
+altura = 1.55m;
+
+bool gostoDeSorvete;
+gostoDeSorvete = true;
+
+char letra;
+letra = 'V';
+
+Console.WriteLine(nome + " " + sobrenome);
+Console.WriteLine(naturalidade);
+Console.WriteLine(idade);
+Console.WriteLine(altura);
+Console.WriteLine(gostoDeSorvete);
+Console.WriteLine(letra);
+
